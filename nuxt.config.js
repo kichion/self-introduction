d581111d0a4a064b29995cb48301d0fdec422b4e
kikuchi kakeru <kakeru.kikuchi@fukurou-labo.co.jp>
2020-06-22 09:48:30 +0900

    :tada: init

---
+++ nuxt.config.js
@@ -0,0 +1,70 @@
+const routerBase =
+  process.env.DEPLOY_ENV === "GH_PAGES"
+    ? {
+        router: {
+          base: "/self-introduction/"
+        }
+      }
+    : {};
+const iconRoot =
+  process.env.DEPLOY_ENV === "GH_PAGES" ? "/self-introduction" : "";
+
+console.log(routerBase);
+
+export default {
+  /*
+   ** Nuxt rendering mode
+   ** See https://nuxtjs.org/api/configuration-mode
+   */
+  mode: "spa",
+  /*
+   ** Headers of the page
+   ** See https://nuxtjs.org/api/configuration-head
+   */
+  head: {
+    title: process.env.npm_package_name || "",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
+    ],
+    link: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: `${iconRoot}/favicon.ico`
+      }
+    ]
+  },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   ** https://nuxtjs.org/guide/plugins
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: ["@nuxt/typescript-build"],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [],
+  /*
+   ** Build configuration
+   ** See https://nuxtjs.org/api/configuration-build/
+   */
+  build: {},
+  /*
+   ** Include Router
+   ** See https://ja.nuxtjs.org/faq/github-pages/
+   */
+  ...routerBase
+};
